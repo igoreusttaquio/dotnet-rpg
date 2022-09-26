825e8d4835cba87586d640a6041ee81f1a8a6758
Igor <igoreusttaquio@gmail.com>
2022-09-26 20:53:44 -0300

    Find character by id, o get an list of characters

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -12,10 +12,25 @@ namespace dotnet_rpg.Controllers
     public class CharacterController : ControllerBase
     {
         private static Character mockCharacter = new Character { Name = "Frodo" };
+        private static List<Character> mockCharacters = new List<Character>{
+            mockCharacter,
+            new Character { Name = "Jinx", Id = 1 , Class = RpgClass.Cleric},
+            new Character { Name = "Lucian", Id = 2 },
+            new Character { Name = "Thresh", Id = 3, Class = RpgClass.Mage}
+        };
+
         [HttpGet]
-        public ActionResult<Character> Get()
+        public ActionResult<List<Character>> Get()
         {
-            return Ok(mockCharacter);
+            return Ok(mockCharacters);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Character> Get(int id)
+        {
+            if (mockCharacters.Any(c => c.Id == id))
+                return Ok(mockCharacters.Find(c => c.Id == id));
+            return NotFound();
         }
     }
 }
