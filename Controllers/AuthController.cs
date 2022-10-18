6d7a56908216c4e366c4a28732fc96ab3349a02f
Igor <igoreusttaquio@gmail.com>
2022-10-18 16:30:08 -0300

    Add authentication controller and DTO user for registration

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_rpg.Data;
+using Microsoft.AspNetCore.Mvc;
+using dotnet_rpg.Services.CaracterService;
+using dotnet_rpg.Dtos.User;
+using dotnet_rpg.Models;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepository;
+        public AuthController(IAuthRepository authRepository)
+        {
+            _authRepository = authRepository;
+
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+        {
+            var response = await _authRepository.Register(
+                new User { Username = request.Username }, request.Password
+            );
+            
+            if (!response.Sucess)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+}
