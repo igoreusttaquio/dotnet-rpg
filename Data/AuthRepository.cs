8ff6f3329c96e7819f2b094e466ef24c20c50dda
Igor <igoreusttaquio@gmail.com>
2022-10-18 15:59:13 -0300

    Save user in database

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -9,14 +9,23 @@ namespace dotnet_rpg.Data
 {
     public class AuthRepository : IAuthRepository
     {
+        private readonly DataContext _context;
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
         public Task<ServiceResponse<string>> Login(string username, string password)
         {
             throw new NotImplementedException();
         }
 
-        public Task<ServiceResponse<int>> Register(User user, string password)
+        public async Task<ServiceResponse<int>> Register(User user, string password)
         {
-            throw new NotImplementedException();
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            ServiceResponse<int> response = new ();
+            response.Data = user.Id;
+            return response;
         }
 
         public Task<bool> UserExists(string username)
